95bddb5b973bfa0dc5d09ea9db911eef647da2fb
Jorge Daret <daret.caravantes@divox.com.mx>
2016-11-08 10:37:24 -0600

    Se cambian campos a mostrar en admin

--- reporte/admin.py
+++ reporte/admin.py
@@ -8,21 +8,10 @@ from datetime import datetime
 # Register your models here.
 class movimientoAdmin(CustomModelAdmin):
   list_per_page = 50
-  list_display=('campo_fecha_registro','folio','matricula','alumno','referencia','descripcion','ciclo','concepto','campo_monto')
+  list_display=('campo_fecha_registro','folio','alumno','referencia','ciclo','concepto','campo_monto')
   list_display_links=('alumno','referencia',)
   list_filter=('alumno','referencia',)
   date_hierarchy='fecha_registro'
-  
-
-  def matricula(self,obj):
-    return obj.alumno.ant+obj.alumno.matricula
-  
-  def descripcion(self,obj):
-    if obj.referencia.descripcion:
-      return obj.referencia.descripcion
-    else:
-      return 'Movimiento Manual'
-    
 
   def campo_monto(self,obj):
     
