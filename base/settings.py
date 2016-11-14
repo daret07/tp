b687059c0cde91148339127ac90b0d22565704ea
Jorge Daret <daret.caravantes@divox.com.mx>
2016-11-14 11:50:29 -0600

    Cambios en settings base de datos

--- base/settings.py
+++ base/settings.py
@@ -90,9 +90,9 @@ DATABASES = {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'fbasica',
+        'NAME': 'fbasicas',
         'USER': 'root',
-        'PASSWORD': '21Divox!=',
+        'PASSWORD': '046797993',
         'HOST': 'localhost',
         'PORT': '3306',
     }
