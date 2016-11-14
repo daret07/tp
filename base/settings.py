c2903c078eb3861e72623d75714691a65bc64740
Jorge Daret <daret.caravantes@divox.com.mx>
2016-11-14 11:51:23 -0600

    settings

--- base/settings.py
+++ base/settings.py
@@ -130,7 +130,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_PATH = ''
+STATIC_PATH = '/dj_fbasicas'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
