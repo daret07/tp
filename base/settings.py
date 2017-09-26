ddab51dc0a13590cff4d39055cd0b093cdd8f7e1
Jorg Daret Caravantes Rodriguez <jorge_daret@MacBook-Pro-de-Jorg.local>
2017-09-26 16:48:06 -0500

    Settings

--- base/settings.py
+++ base/settings.py
@@ -84,14 +84,14 @@ WSGI_APPLICATION = 'base.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        #'ENGINE': 'django.db.backends.mysql', 
-        #'NAME': 'fbasicas',
-        #'USER': 'root',
-        #'PASSWORD': '046797993',
-        #'HOST': 'localhost',
-        #'PORT': '3306',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'perfil-2017',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
